879939d44e91ba4ab3ab4b6ab77358af389ac782
15077693d <oscaryiu.lapsang@gmail.com>
2025-02-10 10:23:41 +0800

    Refactor form components and add UI library with Shadcn/ui styling

--- src/app/visitorComment/_components/visitorComponentForm.tsx
+++ src/app/visitorComment/_components/visitorComponentForm.tsx
@@ -5,25 +5,39 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { Textarea } from "~/components/ui/textarea";
 import { type CreateVisitorCommentInput } from "~/server/api/routers/visitorComment";
 import { api } from "~/trpc/react";
-
 const visitorCommentValidationSchema = z.object({
   name: z.string().min(1, "Name is required"),
   content: z.string().min(1, "Comment cannot be empty"),
 });
 
 export default function VisitorCommentForm() {
-  const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors, isLoading, isSubmitting },
-    reset,
-  } = useForm<CreateVisitorCommentInput>({
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+  const form = useForm<CreateVisitorCommentInput>({
+    defaultValues: {
+      name: "",
+      content: "",
+    },
     resolver: zodResolver(visitorCommentValidationSchema),
   });
+  const keysSchema = visitorCommentValidationSchema.keyof();
+  const {
+    formState: { isLoading, isSubmitting },
+    reset,
+    handleSubmit,
+  } = form;
+  const keys = keysSchema.options[0];
   const utils = api.useUtils();
   const { mutateAsync } = api.visitorComment.create.useMutation({
     onSuccess: async () => {
@@ -37,54 +51,53 @@ export default function VisitorCommentForm() {
   };
 
   return (
-    <div className="max-w-3xl mx-auto font-sans">
-      <DevTool control={control} />
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className=" border-white border p-4"
-      >
-        <div className="mb-6">
-          <input
-            {...register("name")}
-            placeholder="Your name"
-            className="w-full p-3 border border-white bg-transparent   text-base mb-2"
-            disabled={isLoading}
-          />
-          {errors.name && (
-            <span className="text-red-500 text-sm">{errors.name.message}</span>
+    <Form {...form}>
+      <DevTool control={form.control} />
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your Name</FormLabel>
+              <FormControl>
+                <Input
+                  id={keys[0]}
+                  placeholder="Your Name"
+                  {...field}
+                  disabled={isLoading}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
           )}
-        </div>
-
-        <div className="mb-6">
-          <textarea
-            {...register("content")}
-            placeholder="Your comment"
-            className="w-full p-3 border border-white bg-transparent  text-base mb-2 resize-y"
-            disabled={isLoading}
-            rows={4}
-          />
-          {errors.content && (
-            <span className="text-red-500 text-sm">
-              {errors.content.message}
-            </span>
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your Comment</FormLabel>
+              <FormControl>
+                <Textarea
+                  id="content"
+                  placeholder="Your Comment"
+                  {...field}
+                  disabled={isLoading}
+                  rows={4}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
           )}
-        </div>
-
-        <button
-          type="submit"
-          className=" bg-white disabled:cursor-not-allowed  disabled:text-white disabled:bg-black text-black p-4 cursor-pointer text-base transition-colors hover:bg-black hover:text-white disabled:opacity-50"
-          disabled={isSubmitting}
-        >
+        />
+        <Button variant={"secondary"} type="submit" disabled={isSubmitting}>
           {isSubmitting ? "Submitting..." : "Post Comment"}
-        </button>
-        <Link
-          href={"/"}
-          type="submit"
-          className=" bg-white ml-4 text-black p-4 cursor-pointer text-base transition-colors hover:bg-black hover:text-white "
-        >
-          Back
-        </Link>
+        </Button>
+        <Button asChild className="ml-4">
+          <Link href={"/"}>Back</Link>
+        </Button>
       </form>
-    </div>
+    </Form>
   );
 }
